7984fd818c7b3aa2bd8ce99e89197cf8a5b4ff2d
pra17dod <pra17dod@gmail.com>
2021-04-07 12:02:37 +0530

    settings.py: Add googleapiclient in Installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.APIConfig'
+    'api.apps.APIConfig',
+    'googleapiclient',
 ]
 
 MIDDLEWARE = [
