b44e2588622eb2fbf0d68c2662b7e7963e6a0dc4
Aatman Pradhan <aatmansp@rediffmail.com>
2021-04-19 19:52:42 +0530

    POST method added to take json

--- Extension/content.js
+++ Extension/content.js
@@ -1,58 +1,69 @@
 // chrome.runtime.sendMessage({todo:"showActionPage"});
 
-window.addEventListener('load',(e)=>{
-  chrome.storage.sync.set({'status':0})
-})
+window.addEventListener("load", (e) => {
+  chrome.storage.sync.set({ status: 0 });
+});
 
-// var url;
+var url;
 
-async function getData(){
-  
-  let url='http://127.0.0.1:5500/Extension/comment.json';
-    return (await fetch(url)).json();
-   
-    
-  // $.post('/submitJSONData',  // url
-  //      { myData: url }, // data to be submit
-  //      function(data, status, xhr) {   // success callback function
-  //       for(var i=0;i<3;i++){
-  //         if(data.i==1){
-  //           arr.push(i);
-  //         }
-  //       }
-  //           },
-  //      'json');
+async function getData() {
+  // const res=await fetch(urlOfJson)
+  // if(res.json()){
+  //   chrome.runtime.sendMessage({todo:"msg9",message:"NOT Error"});
+  // }else{
+  //   chrome.runtime.sendMessage({todo:"msg9",message:"Error"});
+  // }
+  // return (await fetch(urlOfJson)).json();
+
+  const location = window.location.hostname;
+
+  try {
+    const fetchResponse = await fetch("https://456915cc4cc8.ngrok.io/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: url,
+      }),
+    });
+    if (fetchResponse.ok) {
+      chrome.runtime.sendMessage({ todo: "msg9", message: "NOT Error" });
+      return fetchResponse.json();
+    } else {
+      chrome.runtime.sendMessage({ todo: "msg9", message: "Error" });
+    }
+  } catch (e) {
+    chrome.runtime.sendMessage({ todo: "msg9", message: JSON.stringify(e) });
+  }
 }
 
-
-chrome.runtime.onMessage.addListener(async(req, sender, sendRes) => {
+chrome.runtime.onMessage.addListener(async (req, sender, sendRes) => {
   if (req.todo == "hideComments") {
-    
-    // arr=getData();
-    // url=req.message;
-      
+    url = req.message;
+
     chrome.runtime.sendMessage({ todo: "msg1" });
-    var hidden=[];
-    var data=await getData();
-    data.forEach((element)=>{
-      if(element.value==0){
-        hidden.push(element.Index);
+    var hidden = [];
+    var data = await getData();
+    data.forEach((element) => {
+      if (element.value == 0) {
+        hidden.push(element.index);
       }
-    })
-   
-    chrome.runtime.sendMessage({todo:"msg4",message:hidden});
+    });
+
+    chrome.runtime.sendMessage({ todo: "msg4", message: hidden });
     var msg;
     var x = [];
     var imgSrc = [];
-    var msgs=[];
-    var j=0;
-    var p=0;
+    var msgs = [];
+    var j = 0;
+    var p = 0;
     $("#contents")
       .children()
       .each((i) => {
         msg = $("#content-text").text();
-        // msg=msg.replace(/\s+/g, ''); 
-        msgs[j]=msg;
+        // msg=msg.replace(/\s+/g, '');
+        msgs[j] = msg;
         j++;
         imgSrc[i] = $("#img").attr("src");
         // imgSrc[i]=$('#content-text').text();
@@ -72,13 +83,13 @@ chrome.runtime.onMessage.addListener(async(req, sender, sendRes) => {
       //     chrome.runtime.sendMessage({todo:"msg7",message:true});
       // }
       if (hidden.includes(i)) {
-        chrome.runtime.sendMessage({todo:"msg9",message:msgs[i]});
+        chrome.runtime.sendMessage({ todo: "msg9", message: msgs[i] });
         continue;
       } else {
         p++;
         $("#contents").prepend(x[i]);
       }
-      
+
       // if(hidden.includes(msgs[i])){
       //   chrome.runtime.sendMessage({ todo: "msg6",message:hidden[p] });
       //   // p--;
@@ -88,8 +99,6 @@ chrome.runtime.onMessage.addListener(async(req, sender, sendRes) => {
       // }
     }
     // }, 3000);
-    
-    
   }
 
   if (req.todo == "UnHideComments") {
@@ -97,6 +106,3 @@ chrome.runtime.onMessage.addListener(async(req, sender, sendRes) => {
     location.reload();
   }
 });
-
-
-
