84daf2c8344299a14aabf41bb207ba28de9968f4
pra17dod <pra17dod@gmail.com>
2021-04-20 13:13:47 +0530

    views.py: Add guide and input Json example

--- api/views.py
+++ api/views.py
@@ -10,8 +10,13 @@ import json
 @api_view(['GET','POST'])
 def sentimentAnalyzer(request):
     if request.method == 'GET':
-        msg = "Welcome to Sentiment Analyzer"
-        res = {'message': msg}
+        msg1 = "Welcome to Sentiment Analyzer"
+        msg2 = "This is Json example, you can send as POST request"
+        msg3 = "Change the url value to your youtube video url"        
+        msg4 = "https://www.youtube.com/watch?v=paste_the_link_here"
+        msg5 = "The ML based Analyzer will classify comments as Positive Comment or Negative Comment"
+        
+        res = {'greeting': msg1, 'description':msg2, 'howtouse' :msg3, 'url':msg4, 'working':msg5 }
         return Response(res)
 
     if request.method=="POST":
