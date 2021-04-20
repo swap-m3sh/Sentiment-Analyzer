906219498484da3b8b7f6745865bf4f4ef83e960
pra17dod <pra17dod@gmail.com>
2021-04-20 18:17:41 +0530

    views.py: Alter msg3

--- api/views.py
+++ api/views.py
@@ -12,7 +12,7 @@ def sentimentAnalyzer(request):
     if request.method == 'GET':
         msg1 = "Welcome to Sentiment Analyzer"
         msg2 = "This is Json example, you can send as POST request"
-        msg3 = "Change the url value to your youtube video url"        
+        msg3 = "Copy this Json, change the url value to your youtube video url, paste it below and Click on POST"        
         msg4 = "https://www.youtube.com/watch?v=paste_the_link_here"
         msg5 = "The ML based Analyzer will classify comments as Positive Comment or Negative Comment"
         
