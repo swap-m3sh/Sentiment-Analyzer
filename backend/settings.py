6ed797175a62d94ba325dd96fb5ac8498b8edfd3
pra17dod <pra17dod@gmail.com>
2021-04-21 13:12:18 +0530

    Add heroku configs

--- backend/settings.py
+++ backend/settings.py
@@ -137,3 +137,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+### Heroku Settings
+import django_heroku
+django_heroku.settings(locals())
